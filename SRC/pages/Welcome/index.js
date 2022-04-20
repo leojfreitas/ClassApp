24303b531d7f5b9ab2c1085b27dbb1c44e50ab94
Leonardo Freitas <leonardofreitasjc@hotmail.com>
2022-04-19 23:30:10 -0300

    Second commit

---
+++ SRC/pages/Welcome/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { 
+  View, 
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+
+export default function Welcome() {
+  return (
+    <View style={styles.container}>
+     
+        <View style={styles.containerLogo}> 
+
+          <Image
+          source={require('../../assets/logo.png')}
+          style={{width: '100%'}}
+          resizeMode="contain"
+          />
+          <View style={styles.containerForm}>
+            
+          </View>
+        </View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+
+  }
+})
